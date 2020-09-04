b4f245e6b38a2bab0e43afd193b07fb24e2ff7e0
mcs <s.mcsymiv@gmail.com>
2020-09-04 14:18:30 +0300

    - create demo_app.cs template

---
+++ demo_app/demo_app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demo_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GIT");
+        }
+    }
+}
